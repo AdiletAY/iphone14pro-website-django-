ffd158cd0e471c4e9976f5fc2858815b990d2624
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2023-03-07 17:42:23 +0600

    django

--- sixth/base/views.py
+++ sixth/base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Colors
 
 def home(request):
-    return render(request, 'base/home.html')
+    colors = Colors.objects.all()
+    context = {"colors" : colors}
+    return render(request, 'base/home.html', context)
 
